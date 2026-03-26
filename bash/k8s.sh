9d0e46ccdd5e0478a013ad7d3188b915fddea162
yusufSLCN <yusuf.salcan@criion.org>
2026-03-26 15:54:02 +0100

    Add Kubernetes alias scripts for Bash and Zsh

---
+++ bash/k8s.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# useful aliases for kubernetes / kubectl
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgs='kubectl get svc'
+alias kgd='kubectl get deploy'
+alias kgn='kubectl get nodes -o wide'
+alias kga='kubectl get all'
+alias kctx='kubectl config current-context'
+alias kns='kubectl config view --minify --output "jsonpath={..namespace}" ; echo'
+alias kd='kubectl describe'
+alias kl='kubectl logs'
+alias ke='kubectl exec -it'
+alias kaf='kubectl apply -f'
+alias kdf='kubectl delete -f'
+alias kdelp='kubectl delete pod'
